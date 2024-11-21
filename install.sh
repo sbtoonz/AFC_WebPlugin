2b4d8e74dfe08a0e239149498f4cf27af208fb35
sbtoonz <sbtoonz@gmail.com>
2024-11-21 13:49:09 -0800

    drop moonraker plugin update installer accordingly update .zip for mainsail

--- install.sh
+++ install.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
 # Define paths
-PLUGIN_SRC="moonraker/components/afc_plugin.py"
-PLUGIN_DST="$HOME/moonraker/moonraker/components/afc_plugin.py"
 MAINSAIL_SRC="MainsailworkingCopy.zip"
 FLUIDD_SRC="FluiddworkingCopy.zip"
 MAINSAIL_DST="$HOME/mainsail"
 FLUIDD_DST="$HOME/fluidd"
-MOONRAKER_CONF="$HOME/printer_data/config/moonraker.conf"
 
-# Symlink afc_plugin.py
-if [ -e "$PLUGIN_SRC" ]; then
-    if [ ! -d "$HOME/moonraker/moonraker/components" ]; then
-        echo "Error: Directory $HOME/moonraker/components does not exist. Aborting."
-        exit 1
-    fi
-    echo "Creating symlink for afc_plugin.py..."
-    ln -sf "$PWD/$PLUGIN_SRC" "$PLUGIN_DST"
-    echo "Symlink created."
-else
-    echo "Error: $PLUGIN_SRC not found. Aborting."
-    exit 1
-fi
 
 # User selection for Mainsail or Fluidd
 read -p "Do you want to install Mainsail or Fluidd? (m/f): " USER_CHOICE
@@ -56,24 +40,6 @@ elif [ "$USER_CHOICE" == "f" ] || [ "$USER_CHOICE" == "F" ]; then
     fi
 else
     echo "Invalid choice. Aborting."
-    exit 1
-fi
-
-
-# Install httpx using pip
-echo "Installing httpx using pip..."
-~/moonraker-env/bin/pip install httpx
-if [ $? -eq 0 ]; then
-    echo "httpx installed successfully."
-    echo "Restarting Moonraker service..."
-    if sudo systemctl restart moonraker; then
-        echo "Moonraker restarted successfully."
-    else
-        echo "Error restarting Moonraker. Aborting."
-        exit 1
-    fi
-else
-    echo "Error installing httpx. Aborting."
     exit 1
 fi
 
