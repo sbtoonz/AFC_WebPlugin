12cab8248db81e80dbc6256a84af292c68b6e946
sbtoonz <sbtoonz@gmail.com>
2024-11-21 13:12:16 -0800

    stage changes to AFCPanel.vue Overhaul of data scrape and visual organization items Todo: Add alternative filament reel icons for users outside of the preprovided mainsail/fluidd ones

--- mainsail_src/components/panels/AfcPanel.vue
+++ mainsail_src/components/panels/AfcPanel.vue
@@ -21,51 +21,74 @@
           <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
       </div>
-      <div class="spool-container">
-        <div
-          v-for="(spool, index) in paginatedSpoolData"
-          :key="index"
-          class="spool-card"
-          @click="openChangeSpoolDialog(spool, index)"
-        >
-          <div class="filament-reel" style="padding: 1rem">
-            <svg width="50" height="50" viewBox="0 0 487.04 487.04">
-              <g>
-                <circle
-                  :style="{ fill: spool.color ? `#${spool.color}` : '#999' }"
-                  cx="243.52"
-                  cy="243.52"
-                  r="232.97"
-                />
-                <circle
-                  :style="{ fill: '#e6e6e6' }"
-                  cx="243.52"
-                  cy="243.52"
-                  r="112.5"
-                />
-                <path
-                  :style="{ fill: '#444444' }"
-                  d="M0,243.52c0,134.42,109.1,243.52,243.52,243.52,134.42,0,243.52-109.1,243.52-243.52S377.95,0,243.52,0C109.1,0,0,109.1,0,243.52Zm115.73,181.78c-52.4-39.5-86.52-98.59-94.52-163.72v-.09c-.68-5.43,1-10.89,4.6-15,3.6-4.12,8.79-6.51,14.26-6.57l118.36-1.33c18.99-.21,36.63,9.83,46.12,26.29,9.5,16.45,9.38,36.74-.3,53.09l-60.29,101.76c-2.8,4.73-7.48,8.03-12.87,9.1-5.39,1.06-10.98-.22-15.36-3.52ZM450.22,238.8c5.49,.06,10.7,2.46,14.31,6.59,3.62,4.13,5.3,9.61,4.63,15.06-8.01,65.13-42.12,124.22-94.52,163.72l-.07,.05c-4.37,3.29-9.93,4.57-15.3,3.51-5.37-1.06-10.03-4.36-12.82-9.06l-60.33-101.84c-9.68-16.34-9.8-36.64-.3-53.09,9.5-16.45,27.13-26.5,46.12-26.29l118.27,1.33ZM338.12,40.02c5.04,2.14,8.92,6.32,10.69,11.49,1.77,5.18,1.24,10.86-1.44,15.63l-58.03,103.17c-9.31,16.56-26.83,26.8-45.83,26.8-19,0-36.51-10.25-45.83-26.8l-57.99-103.09c-2.69-4.79-3.22-10.49-1.45-15.69,1.77-5.2,5.68-9.4,10.73-11.54,60.41-25.63,128.64-25.63,189.05,0l.08,.04Z"
-                />
-              </g>
-            </svg>
+      <div
+        v-for="(unit, unitName) in unitsData"
+        :key="unitName"
+        class="unit-section"
+      >
+        <h2 class="unit-title">{{ unitName.replace(/_/g, " ") }}</h2>
+        <div class="hub-status">
+          <span><strong>Hub Status:</strong></span>
+          <span
+            :class="{
+              'status-light': true,
+              'status-green': getHubStatus(unitName),
+              'status-red': !getHubStatus(unitName),
+            }"
+          ></span>
+        </div>
+        <div class="spool-container" style="margin-top: 24px">
+          <div
+            v-for="(spool, index) in unit.spools"
+            :key="index"
+            class="spool-card"
+            @click="openChangeSpoolDialog(spool, index)"
+          >
+            <div class="filament-reel" style="padding: 1rem">
+              <svg width="50" height="50" viewBox="0 0 487.04 487.04">
+                <g>
+                  <circle
+                    :style="{ fill: spool.color ? `#${spool.color}` : '#999' }"
+                    v-if="spool.color"
+                    cx="243.52"
+                    cy="243.52"
+                    r="232.97"
+                  />
+                  <circle
+                    :style="{ fill: '#e6e6e6' }"
+                    cx="243.52"
+                    cy="243.52"
+                    r="112.5"
+                  />
+                  <path
+                    :style="{ fill: '#444444' }"
+                    d="M0,243.52c0,134.42,109.1,243.52,243.52,243.52,134.42,0,243.52-109.1,243.52-243.52S377.95,0,243.52,0C109.1,0,0,109.1,0,243.52Zm115.73,181.78c-52.4-39.5-86.52-98.59-94.52-163.72v-.09c-.68-5.43,1-10.89,4.6-15,3.6-4.12,8.79-6.51,14.26-6.57l118.36-1.33c18.99-.21,36.63,9.83,46.12,26.29,9.5,16.45,9.38,36.74-.3,53.09l-60.29,101.76c-2.8,4.73-7.48,8.03-12.87,9.1-5.39,1.06-10.98-.22-15.36-3.52ZM450.22,238.8c5.49,.06,10.7,2.46,14.31,6.59,3.62,4.13,5.3,9.61,4.63,15.06-8.01,65.13-42.12,124.22-94.52,163.72l-.07,.05c-4.37,3.29-9.93,4.57-15.3,3.51-5.37-1.06-10.03-4.36-12.82-9.06l-60.33-101.84c-9.68-16.34-9.8-36.64-.3-53.09,9.5-16.45,27.13-26.5,46.12-26.29l118.27,1.33ZM338.12,40.02c5.04,2.14,8.92,6.32,10.69,11.49,1.77,5.18,1.24,10.86-1.44,15.63l-58.03,103.17c-9.31,16.56-26.83,26.8-45.83,26.8-19,0-36.51-10.25-45.83-26.8l-57.99-103.09c-2.69-4.79-3.22-10.49-1.45-15.69,1.77-5.2,5.68-9.4,10.73-11.54,60.41-25.63,128.64-25.63,189.05,0l.08,.04Z"
+                  />
+                </g>
+              </svg>
+            </div>
+            <h3>Spool {{ spool.LANE }}</h3>
+            <p v-if="spool.material">
+              <strong>Material:</strong> {{ spool.material }}
+            </p>
+            <p v-if="spool.remaining_weight">
+              <strong>Remaining Weight:</strong>
+              {{ spool.remaining_weight.toFixed(2) }} g
+            </p>
+            <p>
+              <strong>Status:</strong>
+              {{ determineStatus(spool) }}
+              <span
+                :class="{
+                  'status-light': true,
+                  'status-not-ready': determineStatus(spool) === 'Not Ready',
+                  'status-ready': determineStatus(spool) === 'Ready',
+                  'status-in-tool': determineStatus(spool) === 'In Tool',
+                }"
+              >
+              </span>
+            </p>
           </div>
-          <h3>Spool {{ index + 1 + currentPage * spoolsPerPage }}</h3>
-          <p>
-            <strong>Material:</strong>
-            {{ spool.material }}
-          </p>
-          <p>
-            <strong>Remaining Weight:</strong>
-            {{
-              spool.remaining_weight ? spool.remaining_weight.toFixed(2) : "N/A"
-            }}
-            g
-          </p>
-          <p>
-            <strong>Status:</strong>
-            {{ determineStatus(spool) }}
-          </p>
         </div>
       </div>
     </panel>
@@ -106,11 +129,13 @@ export default class AfcPanel extends Mixins(BaseMixin) {
   currentPage: number = 0;
   spoolsPerPage: number = 4;
   index: number = 0;
+  unitsData: any = {};
+
   private lastValidData: any = null;
 
   async mounted() {
     await this.fetchSpoolData();
-    this.intervalId = setInterval(this.fetchSpoolData, 10000); // Refresh data every 10 seconds
+    this.intervalId = setInterval(this.fetchSpoolData, 5);
   }
 
   beforeDestroy() {
@@ -119,75 +144,70 @@ export default class AfcPanel extends Mixins(BaseMixin) {
     }
   }
 
-  private async fetchSpoolData() {
-    try {
-      const response = await fetch("/server/afc/spools", {
-        method: "GET",
-        headers: {
-          "Cache-Control": "no-cache",
-          Pragma: "no-cache",
-        },
-      });
+  fetchSpoolData() {
+    const afcData = this.$store.state.printer.AFC
+      ? JSON.parse(JSON.stringify(this.$store.state.printer.AFC))
+      : null;
 
-      // Handle 200 or 304 response statuses
-      if (response.status === 200) {
-        const data = await response.json();
-        if (
-          data &&
-          data.result &&
-          data.result.status === "success" &&
-          data.result.spools
-        ) {
-          this.lastValidData = data.result.spools;
-          this.systemData = this.lastValidData.system; // Ensure system data is always stored first
-          this.spoolData = this.extractLaneData(this.lastValidData);
-        } else {
-          console.warn("No valid data found in response for spool data.");
+    if (afcData) {
+      this.spoolData = this.extractLaneData(afcData);
+      this.unitsData = this.groupByUnit(this.spoolData); 
+      this.systemData = afcData.system || {}; 
+      for (const unitName in afcData) {
+        if (afcData.hasOwnProperty(unitName) && unitName !== "system") {
+          if (this.unitsData[unitName]) {
+            this.unitsData[unitName].system = afcData[unitName].system || {};
+          }
         }
-      } else if (response.status === 304 && this.lastValidData) {
-        // Use the last valid data if we get a 304 response
-        this.systemData = this.lastValidData.system;
-        this.spoolData = this.extractLaneData(this.lastValidData);
-      } else {
-        console.warn(`Unexpected response status: ${response.status}`);
       }
-    } catch (error) {
-      console.error("Error fetching AFC spool data:", error);
+    } else {
+      this.spoolData = [];
+      this.unitsData = {};
+      this.systemData = {};
     }
   }
 
   extractLaneData(spools: any) {
     const lanes = [];
-    const unitSystemData = {};
-    let globalSystemData = {};
-
-    // Iterate over each unit in the spools data
-    for (const unitName in spools) {
-        if (unitName !== 'system') {
-            const unit = spools[unitName];
-            unitSystemData[unitName] = unit.system || {};
-
-            // Iterate over each lane/leg in the unit
-            for (const laneKey in unit) {
-                // Push each lane/leg data as long as the key is not 'system'
-                if (laneKey !== 'system' && typeof unit[laneKey] === 'object' && unit[laneKey] !== null) {
-                    lanes.push(unit[laneKey]);
-                }
+    if (spools && typeof spools === "object") {
+      for (const unitName in spools) {
+        if (spools.hasOwnProperty(unitName) && unitName !== "system") {
+          const unit = spools[unitName];
+          for (const laneKey in unit) {
+            if (
+              unit.hasOwnProperty(laneKey) &&
+              typeof unit[laneKey] === "object"
+            ) {
+              const laneData = unit[laneKey];
+              laneData.unitName = unitName;
+              laneData.laneName = laneKey;
+              lanes.push(laneData);
             }
-        } else {
-            // Extract global system data
-            globalSystemData = spools[unitName];
+          }
         }
+      }
     }
-
-    // Store or return the extracted data as needed
-    this.unitSystemData = unitSystemData;
-    this.globalSystemData = globalSystemData;
     return lanes;
-}
+  }
+
+  private groupByUnit(spoolData: any[]) {
+    const units: any = {};
+    spoolData.forEach((spool) => {
+      const unitName = spool.unitName;
+      if (!units[unitName]) {
+        units[unitName] = { spools: [] };
+      }
+      units[unitName].spools.push(spool);
+    });
+    return units;
+  }
 
   private get totalPages() {
     return Math.ceil(this.spoolData.length / this.spoolsPerPage);
+  }
+
+  private get unitNames() {
+    return [...new Set(this.spoolData.map((spool) => spool.unitName))];
   }
 
   private get paginatedSpoolData() {
@@ -214,11 +234,11 @@ export default class AfcPanel extends Mixins(BaseMixin) {
         this.systemData &&
         this.systemData.current_load === `leg${spool.LANE}`
       ) {
-        return "Locked and loaded to tool";
+        return "In Tool";
       }
-      return "Locked and Loaded";
+      return "Ready";
     }
-    return "Not Loaded";
+    return "Not Ready";
   }
 
   openChangeSpoolDialog(spool: any, index: number) {
@@ -229,6 +249,13 @@ export default class AfcPanel extends Mixins(BaseMixin) {
       this.selectedLane.laneNumber
     );
     this.showChangeSpoolDialog = true;
+  }
+
+  getHubStatus(unitName) {
+    if (this.unitsData[unitName]?.system?.hub_loaded !== undefined) {
+      return this.unitsData[unitName].system.hub_loaded;
+    }
+    return this.systemData?.hub_loaded || false;
   }
 }
 </script>
@@ -251,24 +278,35 @@ export default class AfcPanel extends Mixins(BaseMixin) {
   padding-bottom: 16px;
 }
 
+.unit-section {
+  margin-bottom: 32px;
+}
+
+.unit-title {
+  font-size: 1.5em;
+  margin-bottom: 16px;
+  text-align: center;
+}
+
 .spool-container {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
-  gap: 16px;
-  padding: 16px;
+  justify-content: center;
+  gap: 8px;
+  padding: 8px;
 }
 
 .spool-card {
   background-color: #2e2e2e;
   border-radius: 8px;
-  padding: 16px;
+  padding: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  max-width: 300px;
+  max-width: 180px;
   width: 100%;
   position: relative;
   cursor: pointer;
   transition: box-shadow 0.3s;
+  margin-bottom: 12px;
 }
 
 .spool-card:hover {
@@ -277,7 +315,7 @@ export default class AfcPanel extends Mixins(BaseMixin) {
 
 .filament-reel {
   position: absolute;
-  top: -20px;
+  top: -30px;
   right: -20px;
 }
 
@@ -294,5 +332,37 @@ export default class AfcPanel extends Mixins(BaseMixin) {
 
 .pagination-controls span {
   margin: 0 16px;
+}
+
+.status-light {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 8px;
+}
+
+.status-green {
+  background-color: rgb(24, 177, 24);
+}
+
+.status-red {
+  background-color: red;
+}
+
+.hub-status {
+  text-align: center;
+}
+
+.status-not-ready {
+  background-color: red;
+}
+
+.status-ready {
+  background-color: rgb(26, 230, 26);
+}
+
+.status-in-tool {
+  background-color: rgb(6, 197, 245);
 }
 </style>
